152cb54260307fd9aee4a1284ce9faaf8cec0737
DeynegaEkaterina <deynega.katena@mail.ru>
2021-04-20 00:26:54 +0300

    fixed CmakeLists

---
+++ include/Element.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ekaterina on 19.04.2021.
+//
+
+#ifndef TEMPLATE_ELEMENT_HPP
+#define TEMPLATE_ELEMENT_HPP
+
+#endif  // TEMPLATE_ELEMENT_HPP
---
+++ include/PicoSHA.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ekaterina on 19.04.2021.
+//
+
+#ifndef TEMPLATE_PICOSHA_HPP
+#define TEMPLATE_PICOSHA_HPP
+
+#endif  // TEMPLATE_PICOSHA_HPP
